686c8bd049e3e41273c7e13055e65eee644ea186
khanjasir90 <khanmohdjasir@gmail.com>
2021-06-04 14:53:33 +0530

    added login.html page and img and templates directory

--- src/app.py
+++ src/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/test")
 def index():
     return "Hello, Moazzam!"
 
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
